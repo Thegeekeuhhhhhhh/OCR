26dc3acb03998c624d66c619d4e2019f34e92a7e
dardan.bytyqi <dardan.bytyqi@epita.fr>
2023-11-04 23:35:12 +0100

    Fixed : bin problems

--- binarisation.h
+++ binarisation.h
@@ -7,8 +7,6 @@
 #include <err.h>
 #include <stdlib.h>
 
-void merge(Uint8* arr, int l, int m, int r);
-void mergeSort(Uint8* arr, int l, int r);
 int median_value(struct my_image* image);
 int weak_edge_handling(Uint8** pixels, int h, int w, int height, int width, int upper);
 Uint8** binarisation(struct my_image* image);
