6d838f1feeccbf2ec6e21f24f44904ce0fd122b8
gregoire le bihan <gregoire.le-bihan@epita.fr>
2023-12-04 00:41:38 +0100

    rota et auto rota

--- solver/rotation.c
+++ solver/rotation.c
@@ -1,99 +1,66 @@
-#include <SDL2/SDL_events.h>
-#include <SDL2/SDL_render.h>
-#include <err.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <stdlib.h>
-#include <string.h>
+#include <math.h>
+#include "helpers.h"
+#include "image.h"
 
-void draw(SDL_Renderer* renderer, SDL_Texture* texture, char* angle)
+/**
+ * @brief Performs a rotation on the coordinates (x, y) by an angle
+ *
+ * @param angle radians
+ * @param x
+ * @param y
+ * @param center_x center of the image on the x axis
+ * @param center_y center of the image on the y axis
+ * @param rx rotated x coordinate
+ * @param ry rotated y coordinate
+ */
+void corrected_rotation_matrix(double angle, double x, double y,
+    double center_x, double center_y, double *rx, double *ry)
 {
-    int rota = SDL_RenderCopyEx(renderer, texture, NULL,
-                  NULL, -atoi(angle), NULL, SDL_FLIP_NONE);
-    if (rota)
-    {
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-    }
-    SDL_RenderPresent(renderer);
+    *rx = (x - center_x) * cos(angle) - (y - center_y) * sin(angle) + center_x;
+    *ry = (x - center_x) * sin(angle) + (y - center_y) * cos(angle) + center_y;
 }
 
+/**
+ * @brief Rotates image counterclockwise by an angle
+ *
+ * @param image image to rotate
+ * @param angle angle of the rotation in degrees
+ */
+Image rotate_image(Image *image, double angle)
+{
+    Image rotated_image = clone_image(image);
+    printf("   🔃 Rotating image by %.0f° (%.5f rad)\n", angle,
+        degrees_to_rad(angle));
 
-    //fait la rotation
-void event_loop(SDL_Renderer* renderer, SDL_Texture* texture,char* angle){
-    SDL_Event event;
-    int test_rota = 0;
-    do
+    angle = degrees_to_rad(angle);
+
+    int w = image->width;
+    int h = image->height;
+    double center_x = (w / (double)2);
+    double center_y = (h / (double)2);
+
+    Pixel black = {0, 0, 0};
+
+    for (int x = 0; x < w; x++)
     {
-        SDL_WaitEvent(&event);
-        switch (event.type)
+        for (int y = 0; y < h; y++)
         {
-        case SDL_QUIT:
-            return;
-        case SDL_WINDOWEVENT:
-            //test changement d'image;
-            if (event.window.event == SDL_WINDOWEVENT_RESIZED && !test_rota)
+            double rx, ry;
+            corrected_rotation_matrix(
+                angle, (double)x, (double)y, center_x, center_y, &rx, &ry);
+
+            if (0 <= rx && rx < w && 0 <= ry && ry < h)
             {
-                draw (renderer, texture, angle);
-                test_rota = 1;
+                Pixel org = image->pixels[(int)rx][(int)ry];
+                Pixel dest = {org.r, org.g, org.b};
+                rotated_image.pixels[x][y] = dest;
             }
-            break;
+            else
+            {
+                rotated_image.pixels[x][y] = black;
+            }
         }
-    }while(1);
+    }
+
+    return rotated_image;
 }
-
-
-int main(int argc, char** argv)
-{
-    //initialisation SDL et cas d'erreur;
-    if (argc != 3)
-        errx(EXIT_FAILURE, "Usage: image-file angle");
-
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-
-    SDL_Window* window = SDL_CreateWindow("Display", 0, 0, 
-        640, 400, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-    if (window == NULL)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 
-        SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-
-    SDL_Texture* texture = IMG_LoadTexture(renderer, argv[1]);
-    if (texture == NULL)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-
-    int w, h;
-    if (SDL_QueryTexture(texture, NULL, NULL, &w, &h) != 0)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-
-    SDL_SetWindowSize(window, w, h);
-
-    event_loop(renderer, texture, argv[2]);
-
-    //save l'image dans un new fichier;
-    //SDL_SetRenderTarget(renderer, texture);
-    //SDL_QueryTexture(texture, NULL, NULL, &w, &h);
-    //SDL_Surface* surface = SDL_CreateRGBSurface(0, w, 
-        //h, 32, 0, 0, 0, 0);
-    //SDL_RenderReadPixels(renderer, NULL, surface->format->format, 
-        //surface->pixels, surface->pitch);
-    //char* outName = argv[1];
-    //outName = strcat("result_", outName);
-    //IMG_SavePNG(surface, outName);
-    //SDL_FreeSurface(surface);
-    //SDL_Texture* target = SDL_GetRenderTarget(renderer);
-    //SDL_SetRenderTarget(renderer, target);
-
-    //Enleve le SDL
-    //SDL_DestroyTexture(texture);
-    //SDL_DestroyRenderer(renderer);
-    //SDL_DestroyWindow(window);
-    //SDL_Quit();
-
-    //return EXIT_SUCCESS;
-}
-
-
